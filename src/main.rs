28fbab5f543d01171ef4573f5cf44a58815264eb
j-brn <me@jbrn.eu>
2020-07-22 22:21:01 +0200

    Add clipboard support

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::collections::HashSet;
-use std::io::{stdin, BufRead};
+use std::io::{stdin, Read};
 use std::iter::FromIterator;
 
-use clipboard::{ClipboardContext, ClipboardProvider};
 use mockbob_core::{Mocker, MockingStrategy, StrategyMocker};
 use structopt::StructOpt;
 
+#[cfg(feature = "clipboard")]
+use copypasta_ext::{prelude::*, x11_fork::ClipboardContext};
+
 #[derive(Debug, StructOpt)]
 struct Cli {
-    #[structopt(long = "strategy", short = "s", default_value = "nth_char")]
+    #[structopt(long = "strategy", short = "s", default_value = "random")]
     pub strategy: String,
     #[structopt(
         long = "nth",
-        short = "n",
         default_value = "2",
         required_if("strategy", "nth_char"),
         help = "overrides the default step of 1 for the nth_char strategy"
@@ -20,23 +21,29 @@ struct Cli {
     pub nth: usize,
     #[structopt(
         long = "probability",
-        short = "p",
         default_value = "0.5",
         required_if("strategy", "random"),
         default_value = "0.5",
         help = "overrides the default probability of 0.5 for the random strategy (0.0 - 1.0)"
     )]
     pub probability: f64,
-    #[structopt(long = "blacklist", short = "b", help = "list of characters to ignore")]
+    #[structopt(long = "blacklist", help = "list of characters to ignore")]
     pub blacklist: Vec<char>,
     #[structopt(
         help = "The actual text to mock. If missing, mockbob-core-cli tries to read from stdin"
     )]
     pub input: Vec<String>,
-    #[structopt(help = "Use the clipboard as input", long = "from_clipboard")]
+    #[structopt(help = "Use the clipboard as input", long = "from-clipboard")]
     pub from_clipboard: bool,
-    #[structopt(help = "Copy the output to the clipboard", long = "to_clipboard")]
+    #[structopt(help = "Copy the output to the clipboard", long = "to-clipboard")]
     pub to_clipboard: bool,
+    #[structopt(
+        help = "don't print to stdout",
+        long = "stdout",
+        long = "quiet",
+        short = "q"
+    )]
+    pub quiet: bool,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -63,31 +70,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         StrategyMocker::new(strategy, blacklist)
     };
 
-    let mut clipboard: ClipboardContext = ClipboardProvider::new().unwrap();
+    let input = {
+        if cli.from_clipboard {
+            #[cfg(not(feature = "clipboard"))]
+            panic!("mockbob was compiled without clipboard support! To enable it, please compile with the 'clipboard' feature enabled");
 
-    let input = if cli.from_clipboard {
-        vec![clipboard.get_contents()?]
-    } else if !cli.input.is_empty() {
-        cli.input
-    } else {
-        stdin()
-            .lock()
-            .lines()
-            .filter_map(|line| line.ok())
-            .collect::<Vec<String>>()
+            #[cfg(feature = "clipboard")]
+            {
+                let mut clipboard = ClipboardContext::new()?;
+                clipboard.get_contents()?
+            }
+        } else if !cli.input.is_empty() {
+            cli.input.join(" ")
+        } else {
+            let mut buf = String::new();
+            stdin().lock().read_to_string(&mut buf)?;
+            buf
+        }
     };
 
-    let output = input
-        .iter()
-        .map(|line| mocker.mock(line))
-        .collect::<Vec<String>>()
-        .join("\n");
+    let output = mocker.mock(&input);
 
-    if cli.to_clipboard {
-        clipboard.set_contents(output.clone())?;
+    if !cli.quiet {
+        println!("{}", &output);
     }
 
-    println!("{}", output);
+    if cli.to_clipboard {
+        #[cfg(not(feature = "clipboard"))]
+        panic!("mockbob was compiled without clipboard support! To enable it, please compile with the 'clipboard' feature enabled");
+
+        #[cfg(feature = "clipboard")]
+        {
+            let mut clipboard = ClipboardContext::new()?;
+            clipboard.set_contents(output)?
+        }
+    }
 
     Ok(())
 }
